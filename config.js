74d89749819156631cca7309ddef9c3200c095ee
mohmaedirgde96 <108673773+mohmaedirgde96@users.noreply.github.com>
2023-08-05 21:51:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const toBool = (x) => x == 'true'
 const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID |inrl~65861QRx7a7c6be1299400cd1821bde4e818| '', //your ssid to run bot
     MONGO_URL : process.env.MONGO_URL,//must be enter your mongo url;
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
